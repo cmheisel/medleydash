1073eb6b4b54d93f8f9db5528cb700ecf524c3aa
Chris Heisel <chris@heisel.org>
2011-04-26 19:09:42 -0400

    Initial template rendering work done.

--- webapp.py
+++ webapp.py
@@ -1,14 +1,32 @@
-from flask import Flask
-app = Flask('medleydash')
+from flask import Flask, render_template
 
 from auth import email, password
 from datasources import login, fetch_feature_data
+
+
+app = Flask('medleydash')
+
+METRIC_NAME_MAPPING = (
+    ('done_this_week', "Features this week"),
+    ('this_weeks_goal', "This week's goal"),
+    ('done_this_month', "Features this month"),
+    ('this_months_goal', "This month's goal"),
+    ('done_last_week', "Last week"),
+    ('done_last_month', "Last month"),
+)
 
 @app.route('/')
 def dashboard():
     connection = login(email, password)
     feature_data = fetch_feature_data(connection)
-    return str(feature_data)
+    metrics = []
+    for key, verbose_name in METRIC_NAME_MAPPING:
+        metrics.append({verbose_name: feature_data[key]})
+
+    context = {
+        'metrics': metrics,
+    }
+    return render_template('dashboard.html', **context)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
