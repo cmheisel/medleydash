805de45fce98c05e1f84b562b46f719da9d30fec
Chris Heisel <chris@heisel.org>
2011-04-26 19:38:51 -0400

    Added caching and timestamp (based on caching)

--- webapp.py
+++ webapp.py
@@ -1,10 +1,15 @@
+import datetime
+
 from flask import Flask, render_template
+from werkzeug.contrib.cache import SimpleCache
 
 from auth import email, password
 from datasources import login, fetch_feature_data
 
 
 app = Flask('medleydash')
+cache = SimpleCache()
+
 
 METRIC_NAME_MAPPING = (
     ('done_this_week', "Features this week"),
@@ -17,14 +22,24 @@ METRIC_NAME_MAPPING = (
 
 @app.route('/')
 def dashboard():
-    connection = login(email, password)
-    feature_data = fetch_feature_data(connection)
+    feature_data = cache.get('medleydash-features')
+    updated_at = cache.get('medleydash-updated')
+    if not feature_data:
+        connection = login(email, password)
+        feature_data = fetch_feature_data(connection)
+        cache.set('medleydash-features', feature_data, timeout=5 * 60)
+        cache.set('medleydash-updated', datetime.datetime.now(),
+                   timeout=5 * 60)
+        updated_at = cache.get('medleydash-updated')
+
     metrics = []
     for key, verbose_name in METRIC_NAME_MAPPING:
         metrics.append({verbose_name: feature_data[key]})
 
     context = {
         'metrics': metrics,
+        'updated_at': updated_at,
+
     }
     return render_template('dashboard.html', **context)
 
