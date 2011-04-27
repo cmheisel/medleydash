a0858b1300d338f77780c8edf560673b1c505210
Chris Heisel <chris@heisel.org>
2011-04-27 19:12:17 -0400

    Added mock data source that can be used for testing

---
+++ datasourcemock.py
@@ -0,0 +1,5 @@
+def login(email, password):
+    return None
+
+def fetch_feature_data(connection):
+    return {'this_months_goal': '60', 'done_this_month': '30', 'done_last_month': '50', 'done_last_week': '9', 'this_weeks_goal': '30', 'done_this_week': '0'}
