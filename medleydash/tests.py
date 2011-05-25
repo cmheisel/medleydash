e0a2ff14aa090ce0e99ec9ed8608280bf3b887c8
Chris Heisel <chris@heisel.org>
2011-05-25 17:07:08 -0400

    Added combined view, removed split views.

--- medleydash/tests.py
+++ medleydash/tests.py
@@ -7,18 +7,12 @@ class DashboardTests(unittest.TestCase):
         from medleydash.webapp import app
         self.app = app.test_client()
 
-    def test_dashboard(self):
+    def test_combined(self):
         rv = self.app.get('/')
         assert 200 == rv.status_code
         assert "<h2>Ave. Cycle Time</h2>" in rv.data
         assert "<title>Medley Development Dashboard</title>" in rv.data
-
-    def test_wip(self):
-    	rv = self.app.get('/progress')
-        assert 200 == rv.status_code
-    	assert "<title>Medley Work In Progress:" in rv.data
         assert "<th>cycletime</th>" in rv.data
-
 
 if __name__ == "__main__":
     unittest.main()
--- medleydash/webapp.py
+++ medleydash/webapp.py
@@ -19,8 +19,7 @@ def get_the_cache():
         cache = SimpleCache()
     return cache
 
-@app.route('/')
-def dashboard():
+def get_metrics_data():
     cache = get_the_cache()
     feature_data = cache.get('medleydash-features')
     updated_at = cache.get('medleydash-updated')
@@ -31,18 +30,9 @@ def dashboard():
         updated_at = datetime.datetime.now()
         cache.set('medleydash-updated', updated_at,
                    timeout=5 * 60)
+    return feature_data, updated_at
 
-    context = {
-        'metrics': feature_data,
-        'updated_at': updated_at,
-        'title': "Medley Development Dashboard",
-        'version': __version__,
-
-    }
-    return render_template('dashboard.html', **context)
-
-@app.route('/progress')
-def wip():
+def get_wip_data():
     cache = get_the_cache()
     wip_data = cache.get('medleydash-wip')
     updated_at = cache.get('medleydash-wip-updated')
@@ -53,15 +43,55 @@ def wip():
         updated_at = datetime.datetime.now()
         cache.set('medleydash-wip-updated', updated_at,
                    timeout=5 * 60)
+    return wip_data, updated_at
+
+#Courtesy http://flask.pocoo.org/snippets/33/
+@app.template_filter()
+def timesince(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+    """
+
+    now = datetime.datetime.now()
+    diff = now - dt
+    
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+        
+        if period:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+
+    return default
+
+@app.route('/')
+def combined():
+    wip_data, wip_updated_at = get_wip_data()
+    metrics_data, metrics_updated_at = get_metrics_data()
+
+    if metrics_updated_at < wip_updated_at:
+        updated_at = metrics_updated_at
+    else:
+        updated_at = wip_updated_at
 
     context = {
-        'title': "Medley Work In Progress: %s" % (len(wip_data), ),
+        'title': 'Medley Development Dashboard',
         'wip_data': wip_data,
         'headers': wip_data[0]._fields,
+        'metrics': metrics_data,
         'updated_at': updated_at,
         'version': __version__,
     }
-    return render_template('wip.html', **context)
+    return render_template('combined.html', **context)
 
 if __name__ == "__main__":
     app.run(debug=True)
